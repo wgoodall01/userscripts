79c137acea306870363ffd0aa5a5ef7a677ccdc0
William Goodall <wgoodall01@gmail.com>
2017-11-19 18:01:50 -0500

    Update libguides-sametab.user.js

--- libguides-sametab.user.js
+++ libguides-sametab.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Libguides Sametab
 // @namespace    http://williamgoodall.tk/
-// @version      0.1
+// @version      0.2
 // @description  On libguides.mka.org, make all target=_blank links open in the same tab.
 // @author       William Goodall
 // @match        http://libguides.mka.org/*
@@ -11,4 +11,13 @@
 (function() {
     // Remove annoying new-tab links
     document.querySelectorAll('a[target="_blank"]').forEach(e => e.target = "");
+
+    // Log in automatically with password for research.
+    if(window.location.href.startsWith("http://libguides.mka.org/databases/")){
+        const pwInput = document.getElementById("password");
+        if(pwInput !== null){
+            // submitPassword() is part of libguides, and this is janky.
+            setTimeout(() => submitPassword(), 500);
+        }
+    }
 })();
