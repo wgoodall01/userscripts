f8ba90b30ce64eace6f6724eb8228abe917b6df5
William Goodall <wgoodall01@gmail.com>
2018-04-07 14:19:44 -0400

    moodle-search: card, collapse headers, look nice.

--- moodle-search.user.js
+++ moodle-search.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Moodle Search
 // @namespace    http://williamgoodall.tk/
-// @version      0.1
-// @description  Search all Moodle items on a page.
+// @version      0.2
+// @description  Search all Moodle items on a page, and collapse the header.
 // @author       William Goodall
 // @match        https://moodle.mka.org/course/view.php*
 // @grant        none
@@ -13,54 +13,69 @@
 
   const main = document.createElement('div');
   main.innerHTML = `
+<div class="mdc-card">
+    <details>
+      <summary id="mdc-title"></summary>
+    </details>
+    <input id="mdc-input" placeholder="Search"/>
+    <div id="mdc-results"></div>
+  </div>
 
 <style>
-.sch_container{
-    padding:8px;
-    border-radius:5px;
-    border: 1px solid lightgrey;
-    margin-bottom:20px;
+.mdc-card{
+  box-sizing:border-box;
+  padding:30px;
+  background-color:white;
+  box-shadow:0 0 20px 0 rgba(0,0,0,0.2);
+  margin-bottom:50px;
+  border-radius:5px;
 }
 
-.sch_bar{
-    display:flex;
-    flex-direction:row;
-    align-items:center;
+.mdc-card *{
+  box-sizing:inherit;
 }
 
-.sch_input{
-    padding:5px;
-    flex:1;
-    border:none;
-    background:none;
+.mdc-card summary{
+  font-size:30px;
+  margin-bottom:20px;
 }
 
-.sch_bar .fa{
-    margin-left:5px;
+.mdc-card input{
+  width:100%;
+  border:none;
+  font-size:16px;
+  padding:5px;
+  padding-left:0;
+  border-bottom:1px solid #dedede;
 }
 
-.sch_results .activityinstance{
-    margin-left:5px;
-    margin-bottom:5px;
-    margin-top:5px;
+.mdc-card input:focus{
+  outline:none;
+  border-bottom:1px solid black;
 }
 
+#mdc-results:not(:empty){
+  margin-top:20px;
+}
+
+#mdc-results>div{
+  margin-top:5px;
+}
 </style>
-
-<div class="sch_container">
-    <div class="sch_bar">
-        <input placeholder="Search" id="sch_input" class="sch_input"></input>
-        <i class="fa fa-search"></i>
-    </div>
-    <div class="sch_results" id="sch_results">
-
-    </div>
-</div>
-
 `;
-  if (document.querySelector('#sch_input') == null) {
-    document.querySelector('.content').prepend(main);
+  if (document.querySelector('#mdc-input') == null) {
+    document.querySelector('.course-content ul').prepend(main);
 
+    document.querySelector("#mdc-title").textContent = document.querySelector(".page-header-headings h1").textContent;
+
+    // Transplant uncollapsable first topic block
+    const headBlock = document.querySelector("#section-0");
+    if(headBlock != null){
+      headBlock.parentNode.removeChild(headBlock);
+      document.querySelector(".mdc-card details").appendChild(headBlock);
+    }
+
+    // Search match predicate
     const matches = (text, filter) => {
       filter = filter.trim().toLowerCase();
       text = text.trim().toLowerCase();
@@ -77,8 +92,9 @@
       }
     };
 
-    const input = document.querySelector('#sch_input');
-    const results = document.querySelector('#sch_results');
+    // Wire up search to page
+    const input = document.querySelector('#mdc-input');
+    const results = document.querySelector('#mdc-results');
     input.oninput = e => {
       const filter = e.target.value;
       const allMods = document.querySelectorAll('.activity .activityinstance');
