ebc7fec5fea94f48cda968278a4787d67509ff6b
William Goodall <wgoodall01@gmail.com>
2018-09-06 16:31:59 -0400

    Bump moodle-dispatch version

--- moodle-dispatch.user.js
+++ moodle-dispatch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Moodle Dispatch
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://moodle.mka.org/*
